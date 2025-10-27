789658f2801768e5e044aa73abd0577e75d3e783
unknown <heinhtetaung5698@gmail.com>
2025-10-28 00:45:53 +0700

    home part3 and aboout us 3 together

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
+"use client";
+import Home3 from "./components/home3/Home3";
+import About_us3 from "./components/about_us/about_us3";
 export default function Home(){
   return(
-    <div>This is Home Page</div>
+    <div>This is Home Page
+      <Home3/>
+
+    </div>
   )
 }
