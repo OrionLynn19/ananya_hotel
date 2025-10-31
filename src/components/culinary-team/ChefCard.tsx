a93fc97073dad8f95511fadd290d4b5e4ec4c46b
asett12 <158172308+asett12@users.noreply.github.com>
2025-10-31 11:27:34 +0700

    resAndhealth-3:fixed typography and adjusted layout for desktop

--- src/components/culinary-team/ChefCard.tsx
+++ src/components/culinary-team/ChefCard.tsx
@@ -2,6 +2,19 @@
 
 import Image from "next/image";
 import { motion } from "framer-motion";
+import { Montserrat, Poltawski_Nowy } from "next/font/google";
+
+const montserrat = Montserrat({
+  subsets: ["latin"],
+  weight: ["400", "600", "700"],
+  display: "swap",
+});
+
+const poltawskiNowy = Poltawski_Nowy({
+  subsets: ["latin"],
+  weight: ["400", "600", "700"],
+  display: "swap",
+});
 
 export type Chef = {
     id: string;
@@ -53,18 +66,22 @@ export default function ChefCard({
             {/* overlay */}
             <motion.div
                 className={`
-                    absolute inset-x-0 bottom-0 bg-black/80 text-white
+                    absolute inset-x-0 bottom-0 bg-black/40 backdrop-blur text-white
                     flex flex-col items-center justify-center text-center
                     px-6
                 `}
-                animate={{ height: isActive ? 150 : 120 }}
+                animate={{ height: isActive ? 170 : 140 }}
                 transition={{ type: "spring", stiffness: 200, damping: 25 }}
             >
-                <p className="font-semibold text-2xl leading-tight">{chef.name}</p>
+                <p className={`font-bold md:text-[40px] ${poltawskiNowy.className} 
+                                bg-gradient-to-b from-[#FCF9F6] to-[#969492]
+                                bg-clip-text text-transparent leading-tight`}>
+                                    {chef.name}
+                </p>
 
                 {isActive && (
                     <motion.p
-                        className="mt-3 text-sm md:text-base text-left text-white/90 leading-snug w-full"
+                        className={`mt-3 md:text-[18px] font-semibold md:text-base text-left text-white/90 ${montserrat.className} w-full`}
                         initial={{ opacity: 0, y: 10 }}
                         animate={{ opacity: 1, y: 0 }}
                         exit={{ opacity: 0, y: 10 }}
