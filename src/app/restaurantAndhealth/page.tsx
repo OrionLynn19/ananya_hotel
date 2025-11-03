b4620efd25628c76d2f72fdec52b93e52cbcdc5e
unknown <heinhtetaung5698@gmail.com>
2025-11-03 19:18:35 +0700

    Res&health section2

--- src/app/restaurantAndhealth/page.tsx
+++ src/app/restaurantAndhealth/page.tsx
@@ -1,9 +1,11 @@
 import CulinaryTeamSection from "@/components/culinary-team/CulinaryTeamSection";
+import DiningShowcase from "@/components/dinning/DinningExandable";
 
 export default function AboutPage() {
   return (
     <>
       <CulinaryTeamSection />
+      < DiningShowcase/>
     </>
   );
 }
