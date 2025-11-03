9bc0e0da12c2f4ae04533c08ff494a58975cd374
asett12 <158172308+asett12@users.noreply.github.com>
2025-11-03 12:08:35 +0700

    ResSlider:completed desktop layout

--- src/app/restaurantAndhealth/page.tsx
+++ src/app/restaurantAndhealth/page.tsx
@@ -1,8 +1,10 @@
+import CulinarySlider from "../../components/CulinarySlider";
 import CulinaryTeamSection from "@/components/culinary-team/CulinaryTeamSection";
 
 export default function AboutPage() {
   return (
     <>
+      <CulinarySlider />
       <CulinaryTeamSection />
     </>
   );
