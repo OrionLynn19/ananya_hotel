6aa3c70609c9600f414b0228ad5235c4cb9467b6
Thidar Aung <thidaraung00iii@gmail.com>
2025-11-11 23:18:16 +0700

    feat:faq3(faqpage)

--- src/app/faqandcontactus/page.tsx
+++ src/app/faqandcontactus/page.tsx
@@ -1,5 +1,13 @@
-" use client" ; 
+" use client" ;
+import FAQ3 from "../components/Faq3";
+
+ 
 
 export default function FaqAndContactUs () {
-    return <div> This is Faq And Contact Us Page </div>   
+    return (
+        <div>
+            <div> This is Faq And Contact Us Page </div>  
+            <div><FAQ3 /></div>
+        </div>
+    ) 
 }
