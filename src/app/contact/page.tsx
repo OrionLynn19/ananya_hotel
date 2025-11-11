f7b34730686dcca82255a00a3221df8bf1f1d0fa
Kyaw Zan Lin <kyawzanlinn95@gmail.com>
2025-11-11 11:52:01 +0700

    done

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,21 +1,16 @@
 "use client";
 
 import FaqHero from "../components/FaqHero";
-import FaqAccordion from "../components/FaqAccordion"; // ← add this
+import FaqAccordion from "../components/FaqAccordion";
+import { FAQ_DATA } from "@/app/data/faq.data"; 
 
 export default function Contact() {
   return (
     <main className="bg-white overflow-x-hidden">
       <FaqHero />
 
-     
-      <section className="pt-10 md:pt-14">
-        <FaqAccordion />
-      </section>
-
-      
       <section className="mx-auto max-w-5xl px-6 py-16 md:py-20">
-       
+        <FaqAccordion items={FAQ_DATA} />
       </section>
     </main>
   );
