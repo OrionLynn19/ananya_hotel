b5d1ead2c53c85136e3574254040036c21546ebd
Kyaw Zan Lin <kyawzanlinn95@gmail.com>
2025-11-14 14:42:24 +0700

    Changepage

--- src/app/faqandcontactus/page.tsx
+++ src/app/faqandcontactus/page.tsx
@@ -1,5 +1,14 @@
 " use client" ; 
-
+import FaqHero from "../components/FaqHero";
+import FaqAccordion from "../components/FaqAccordion";
+import { FAQ_DATA } from "@/app/data/faq.data"; 
 export default function FaqAndContactUs () {
-    return <div> This is Faq And Contact Us Page </div>   
+    return <div> <main className="bg-white overflow-x-hidden">
+      <FaqHero />
+
+      <section className="mx-auto max-w-5xl px-6 py-16 md:py-20">
+        <FaqAccordion items={FAQ_DATA} />
+      </section>
+    </main>
+     </div>   
 }
